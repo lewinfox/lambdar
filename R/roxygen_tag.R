6210d3ea15e4292f14b769baddbf70450fb0931f
Lewin Appleton-Fox <lewin.a.f@gmail.com>
2021-08-11 18:27:49 +1200

    Test for @lambda tag detection
    Previously we were picking up anything with a roxygen block

--- R/roxygen_tag.R
+++ R/roxygen_tag.R
@@ -69,14 +69,52 @@ lam_parse_project_handlers <- function() {
   files <- list.files(path = lam_proj_path(), pattern = "\\.R$", all.files = FALSE)
   res <- character()
   for (file in files) {
-    parsed <- roxygen2::parse_file(file)
-    res <- c(
-      res,
-      sapply(
-        parsed,
-        function(lambda) paste(gsub("\\.[Rr]$", "", lambda$file), lambda$object$alias, sep = ".")
-      )
-    )
+    # TODO: Do this without using c() all the time. Not the end of the world as we don't expect
+    #       large numbers of lambda functions, but this is still a bit gross.
+    res <- c(res, lam_parse_file_handlers(file))
   }
   res
+}
+
+#' Parse files containing `@lambda` tags into AWS Lambda handler specifiers
+#'
+#' AWS Lambda handlers are specified as `file.function_name`.
+#'
+#' @param file A file path
+#'
+#' @return Character vector containing `"file.function_name"` for any `@lambda`-tagged functions in
+#'   `file`.
+#'
+#' @keywords internal
+lam_parse_file_handlers <- function(file) {
+  parsed <- roxygen2::parse_file(file)
+  has_lambda_tag <- sapply(parsed, lam_roxy_block_has_lambda_tag)
+  if (!any(has_lambda_tag)) {
+    return(invisible(NULL))
+  }
+  parsed <- parsed[has_lambda_tag]
+  sapply(
+    parsed,
+    function(block) paste(gsub("\\.[Rr]$", "", block$file), block$object$alias, sep = ".")
+  )
+}
+
+#' Does a parsed roxygen block contain a `@lambda` tag?
+#'
+#' @param block A roxy_block from [roxygen2::parse_file()]
+#'
+#' @return `TRUE` if the block contains a `@lambda` tag, `FALSE` otherwise.
+#'
+#' @keywords internal
+lam_roxy_block_has_lambda_tag <- function(block) {
+  # TODO: Stop suppressing warnings like this! Not sure what it is that generates them, probably
+  #       need to read the docs
+  #       (https://cran.r-project.org/web/packages/roxygen2/vignettes/extending.html)
+  #'      to understand how to use this properly, but it works for now.
+  tryCatch(
+    {
+      any(sapply(block$tags, function(tag) tag$tag == "lambda"))
+    },
+    warning = function(e) {}
+  )
 }
--- tests/testthat/test-roxygen_tag.R
+++ tests/testthat/test-roxygen_tag.R
@@ -1,4 +1,9 @@
 test_that("`@lambda` tag is parsed correctly", {
+
+  if (!requireNamespace("withr", quietly = TRUE)) {
+    skip("Package `withr` is required but not installed")
+  }
+
   text <- "
   #' @lambda
   f <- function(x, y) {
@@ -9,7 +14,77 @@ test_that("`@lambda` tag is parsed correctly", {
   g <- function() {
     FALSE
   }
+
+  #' More complete documentation
+  #'
+  #' This is longer than the short docs above
+  #'
+  #' @param foo A bar
+  #'
+  #' @return A baz
+  #'
+  #' @export
+  #'
+  #' @lambda
+  foobar <- function(foo) {
+    baz(foo)
+  }
   "
-  # Roxygen should have no trouble parsing this code
-  expect_length(roxygen2::parse_text(text), 2)
+
+  # `lam_parse_file_handlers()` should return a vector of "file.function_name"
+  withr::with_tempfile(
+    "tmp",
+    {
+      cat(text, file = tmp)
+      expect_equal(lam_parse_file_handlers(tmp), paste(tmp, c("f", "g", "foobar"), sep = "."))
+    }
+  )
+
+})
+
+test_that("functions without the `@lambda` tag are not detected", {
+
+  if (!requireNamespace("withr", quietly = TRUE)) {
+    skip("Package `withr` is required but not installed")
+  }
+
+  text <- "
+  #' No @lambda tag
+  #'
+  #' @param foo A bar
+  #'
+  #' @export
+  f <- function(foo) {
+    bar(foo)
+  }
+
+
+  # No docs of any kind
+
+  g <- function() {
+    FALSE
+  }
+
+  # A non-compliant roxygen block
+
+  # @lambda
+  barbaz <- function() {
+    FALSE
+  }
+
+  #' This is the only one we should find
+  #'
+  #' @lambda
+  findme <- function() {
+    TRUE
+  }
+  "
+
+  withr::with_tempfile(
+    "tmp",
+    {
+      cat(text, file = tmp)
+      expect_equal(lam_parse_file_handlers(tmp), paste(tmp, c("findme"), sep = "."))
+    }
+  )
 })
