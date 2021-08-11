40a6c0c56146ed1bc027f22fd2af887f9651109e
Lewin Appleton-Fox <lewin.a.f@gmail.com>
2021-08-11 17:02:23 +1200

    Rearrange `utils.R`

--- R/utils.R
+++ R/utils.R
@@ -1,19 +1,4 @@
-#' Tidy file paths by removing instances of `dir` from `x`.
-#'
-#' Ensures that we get a relative path for `x`. Pinched from `rlang:::relish()`.
-#'
-#' @param x File path
-#' @param dir Directory to strip from `x`
-#'
-#' @return Character vector
-#'
-#' @keywords internal
-relish <- function(x, dir = usethis::proj_get()) {
-  if (substr(dir, nchar(dir), nchar(dir)) != "/") {
-    dir <- paste0(dir, "/")
-  }
-  gsub(dir, "", x, fixed = TRUE)
-}
+# ---- Dockerfile formatting helpers ----
 
 #' Convert a vector into a list
 #'
@@ -30,7 +15,7 @@ lam_build_quoted_list <- function(items = NULL) {
   if (is.null(items)) {
     return(NULL)
   }
-  glue::glue_collapse(glue::double_quote(items), sep = ",")
+  glue::glue_collapse(glue::double_quote(items), sep = ", ")
 }
 
 #' @describeIn build-lists Build an unquoted space-separated list
@@ -48,54 +33,6 @@ lam_build_space_separated_list <- function(items = NULL) {
 lam_r_version <- function() {
   paste0(R.version$major, ".", R.version$minor)
 }
-
-#' Do we have docker installed?
-#'
-#' @keywords internal
-lam_has_docker <- function() {
-  x <- Sys.which("docker")
-  if (x == "") {
-    msg <- paste(
-      "No docker installation found.",
-      "Refer to {.url https://docs.docker.com/get-docker/} for installation instructions"
-    )
-    cli::cli_alert_warning(msg)
-    return(FALSE)
-  }
-  TRUE
-}
-
-#' Path utilities
-#'
-#' Pinched from `usethis`.
-#'
-#' @param ... Path elements
-#'
-#' @keywords internal
-lam_proj_path <- function(...) {
-  relish(file.path(usethis::proj_get(), ...))
-}
-
-#' @describeIn lam_proj_path Path to the `lambdar/` directory
-lam_dir_path <- function() {
-  lam_proj_path(".lambdar")
-}
-
-#' @describeIn lam_proj_path Path to lambdar config file
-lam_config_file <- function() {
-  lam_proj_path("_lambdar.yml")
-}
-
-#' @describeIn lam_proj_path Path to Dockerfile
-lam_dockerfile_path <- function() {
-  lam_proj_path("Dockerfile")
-}
-
-#' @describeIn lam_proj_path Path to runtime function
-lam_runtime_path <- function() {
-  lam_proj_path(lam_dir_path(), "lambdar_runtime.R")
-}
-
 
 #' Create a space-separated list of environment variables
 #'
@@ -115,4 +52,73 @@ lam_build_env_list <- function(env = list()) {
   }
   vars = sapply(names(env), function(name) paste0(toupper(name), "=\"", env[[name]], "\""))
   lam_build_space_separated_list(vars)
+}
+
+
+# ---- Check Docker ----
+
+#' Do we have docker installed?
+#'
+#' @keywords internal
+lam_has_docker <- function() {
+  x <- Sys.which("docker")
+  if (x == "") {
+    msg <- paste(
+      "No docker installation found.",
+      "Refer to {.url https://docs.docker.com/get-docker/} for installation instructions"
+    )
+    cli::cli_alert_warning(msg)
+    return(FALSE)
+  }
+  TRUE
+}
+
+# ---- Path utilities ----
+
+#' Path utilities
+#'
+#' Pinched from `usethis`.
+#'
+#' @param ... Path elements
+#'
+#' @keywords internal
+lam_proj_path <- function(...) {
+  relish(file.path(usethis::proj_get(), ...))
+}
+
+#' @describeIn lam_proj_path Path to the `.lambdar/` directory
+lam_dir_path <- function() {
+  lam_proj_path(".lambdar")
+}
+
+#' @describeIn lam_proj_path Path to `_lambdar.yml` config file
+lam_config_file <- function() {
+  lam_proj_path("_lambdar.yml")
+}
+
+#' @describeIn lam_proj_path Path to Dockerfile
+lam_dockerfile_path <- function() {
+  lam_proj_path("Dockerfile")
+}
+
+#' @describeIn lam_proj_path Path to runtime function
+lam_runtime_path <- function() {
+  lam_proj_path(lam_dir_path(), "lambdar_runtime.R")
+}
+
+#' Tidy file paths by removing instances of `dir` from `x`.
+#'
+#' Ensures that we get a relative path for `x`. Pinched from `rlang:::relish()`.
+#'
+#' @param x File path
+#' @param dir Directory to strip from `x`
+#'
+#' @return Character vector
+#'
+#' @keywords internal
+relish <- function(x, dir = usethis::proj_get()) {
+  if (substr(dir, nchar(dir), nchar(dir)) != "/") {
+    dir <- paste0(dir, "/")
+  }
+  gsub(dir, "", x, fixed = TRUE)
 }
