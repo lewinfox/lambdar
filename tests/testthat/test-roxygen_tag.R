1ffc4c751c16ecce1df3fc6614efaa3370d78081
Lewin Appleton-Fox <lewin.a.f@gmail.com>
2021-08-11 17:42:29 +1200

    Simplify tests

--- tests/testthat/test-roxygen_tag.R
+++ tests/testthat/test-roxygen_tag.R
@@ -9,17 +9,7 @@ test_that("`@lambda` tag is parsed correctly", {
   g <- function() {
     FALSE
   }
-"
-  # Testing text processing
-  roclet_proc_text_result <- roxygen2::roc_proc_text(roclet_lambda(), text)
-
-  expect_type(roclet_proc_text_result, "list")
-  expect_length(roclet_proc_text_result, 2)
-
-  # Testing processing the output of the function above
-  roclet_output_result <- suppressMessages(roxygen2::roclet_output(roclet_lambda(), roclet_proc_text_result))
-
-  expect_type(roclet_output_result, "list")
-  expect_length(roclet_output_result, 2)
-  expect_message(roxygen2::roclet_output(roclet_lambda(), roclet_proc_text_result), "I got a lambda roclet")
+  "
+  # Roxygen should have no trouble parsing this code
+  expect_length(roxygen2::parse_text(text), 2)
 })
