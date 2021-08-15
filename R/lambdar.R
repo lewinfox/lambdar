8479814391ef9f4517624ec7e842f6b1cd1d9523
Lewin Appleton-Fox <lewin.a.f@gmail.com>
2021-08-15 18:51:19 +1200

    Rewrite `lambdar.R`

--- R/lambdar.R
+++ R/lambdar.R
@@ -1,3 +1,137 @@
+#' Use lambdar with your project
+#'
+#' @section Description:
+#' Call this function to create the necessary files lambdar needs to work. Specifically:
+#'
+#' * A `_lambdar.yml` config file
+#' * A `lambdar/` directory to house a copy of the R runtime we will load into your container
+#'
+#' The function will try and pre-populate your config file with as much information as possible, but
+#' you are free to amend as much or as little as you like.
+#'
+#' @export
+use_lambdar <- function() {
+  # If this fails we want to restore the dir to its previous state
+  LAMBDAR_DIR <- lam_dir_path()
+  LAMBDAR_RUNTIME_PATH <- lam_runtime_path()
+  LAMBDAR_CONFIG_PATH <- lam_config_path()
+
+  tryCatch(
+    {
+      if (!dir.exists(LAMBDAR_DIR)) {
+        dir.create(LAMBDAR_DIR)
+        cli::cli_alert_success("Creating {.path {LAMBDAR_DIR}} directory")
+      }
+      file.copy(
+        system.file("runtime", "lambdar_runtime.R", package = "lambdar", mustWork = TRUE),
+        LAMBDAR_RUNTIME_PATH
+      )
+      cli::cli_alert_success("Writing {.path {LAMBDAR_RUNTIME_PATH}}")
+      build_config()
+    },
+    error = function(e) {
+      if (dir.exists(LAMBDAR_DIR)) {
+        unlink(LAMBDAR_DIR, recursive = TRUE, force = TRUE)
+        cli::cli_alert_warning("Removing {.path {LAMBDAR_DIR}} directory")
+      }
+      if (file.exists(LAMBDAR_CONFIG_PATH)) {
+        unlink(LAMBDAR_CONFIG_PATH, "_lambdar.yml", force = TRUE)
+        cli::cli_alert_warning("Removing {.path {LAMBDAR_CONFIG_PATH}}")
+      }
+      stop(e)
+    }
+  )
+}
+
+
+#' Build a `_lambdar.yml` file
+#'
+#' Scans your project folder for `.R` files, identified those containing `@lambda` tags, and writes
+#' a `_lambdar.yml` config file containing relevant information.
+#'
+#' @export
+build_config <- function() {
+  # Parse out the handlers ("file.function") and then generate a unique list of "file.R" names
+  handlers <- lam_parse_project_handlers()
+  include_files <- unique(
+    sapply(
+      strsplit(handlers, ".", fixed = T),
+      function(f) paste0(f[[1]], ".R")
+    )
+  )
+
+  # TODO: Look for source() calls and follow that graph.
+
+  # These are parameters that are not set in the default config. They also need to be formatted
+  # before we pass them to `usethis::use_template()`
+  extra_params <- list(
+    lambda_handlers = lam_build_quoted_list(handlers),
+    include_files = lam_build_quoted_list(include_files)
+  )
+
+  cfg <- new_lambdar_config(extra_params)
+
+  usethis::use_template(
+    "_lambdar.yml",
+    save_as = "_lambdar.yml",
+    data = cfg,
+    package = "lambdar",
+    open = TRUE
+  )
+}
+
+#' Create a Dockerfile
+#'
+#' Writes a Dockerfile to your project root directory based on a configuration object. If no
+#' config object is provided it will try and read `_lambdar.yml` from your project's root directory.
+#'
+#' @param cfg A [lambdar_config()] object (optional).
+#' @param quiet If `FALSE`, will tell you the command to build your container.
+#'
+#' @export
+build_dockerfile <- function(cfg = NULL, quiet = FALSE) {
+  tryCatch(
+    {
+      # If this is called in standalone mode, read the config file. Otherwise we expect to have a
+      # `cfg` object passed in
+      if (is.null(cfg)) {
+        cfg <- lambdar_config_from_file(lam_config_path())
+      }
+
+      # Validate the config object
+      if (!is_lambdar_config(cfg)) {
+        msg <- "{.var cfg} must be a {.code lambdar_config} object. See {.code ?lambdar_config}."
+        cli::cli_alert_danger(msg)
+        rlang::abort("Invalid configuration object")
+      }
+
+      # Some elements of the config file need formatting before they're inserted into the Dockerfile
+      # template
+      cfg$r_packages      <- lam_build_quoted_list(cfg$r_packages)
+      cfg$r_package_repos <- lam_build_quoted_list(cfg$r_package_repos)
+      cfg$linux_packages  <- lam_build_space_separated_list(cfg$linux_packages)
+      cfg$include_files   <- lam_build_quoted_list(cfg$include_files)
+      cfg$env             <- lam_build_env_list(cfg$env)
+      cfg$r_runtime_file  <- lam_runtime_path()
+
+      # Replace any zero-length elements with NULL to prevent the populating the Dockerfile
+      cfg <- lapply(cfg, function(item) if (length(item) > 0) item else NULL)
+
+      # Build and write the Dockerfile
+      usethis::use_template("Dockerfile", save_as = "Dockerfile", data = cfg, package = "lambdar")
+      if (!quiet) {
+        msg <- paste("To build your container, run {.code docker build -t {cfg$app_name} .}",
+                     "or {.code lambdar::build_container()}")
+        cli::cli_alert_info(msg)
+      }
+    },
+    lambdar_no_config = function(e) {
+      return(invisible())
+    }
+  )
+}
+
+
 #' Build a container image from a config file
 #'
 #' Reads your `_lambdar.yml` config file, creates a Dockerfile from it, and then attempts to create
@@ -25,151 +159,30 @@ build_container <- function() {
   }
 
   # Read config and create the Dockerfile
-  cfg <- lambdar_config(lam_config_file())
+  cfg <- lambdar_config_from_file(lam_config_path())
   build_dockerfile(cfg, quiet = TRUE)
 
   # Build the container
-  docker_build_cmd <- glue::glue("docker build -t {cfg$name} .")
+  docker_build_cmd <- glue::glue("docker build -t {cfg$app_name} .")
   exit_code <- system(docker_build_cmd)
 
   # Check for success
   if (exit_code != 0) {
-    cli::cli_alert_danger(paste("Docker build failed with exit code", exit_code))
     rlang::abort("Docker build failure")
   }
 
   # Let the user know we are OK and provide useful URLs for testing
   cli::cli_alert_success("Docker build successful")
   if (length(cfg$lambda_handler) > 1) {
-    cli::cli_alert_info("To start your container run {.code docker run -p 9000:8080 {cfg$name} <handler>}")
+    msg <- "To start your container run {.code docker run -p 9000:8080 {cfg$name} <handler>}"
+    cli::cli_alert_info(msg)
     cli::cli_alert_info("Possible values of {.code <handler>} are {.code {cfg$lambda_handler}}")
   } else {
-    cli::cli_alert_info("To start your container run {.code docker run -p 9000:8080 {cfg$name} {cfg$lambda_handler}}")
+    msg <- paste("To start your container run" ,
+                 "{.code docker run -p 9000:8080 {cfg$name} {cfg$lambda_handler}}")
+    cli::cli_alert_info(msg)
   }
-  cli::cli_alert_info("Once running you can send test queries to {.code http://localhost:9000/2015-03-31/functions/function/invocations}")
-}
-
-#' Create a Dockerfile
-#'
-#' Writes a Dockerfile to your project root directory based on a configuration object. If no
-#' config object is provided it will try and read `_lambdar.yml` from your project's root directory.
-#'
-#' @param cfg A [lambdar_config()] object (optional).
-#' @param quiet If `FALSE`, will tell you the command to build your container.
-#'
-#' @export
-build_dockerfile <- function(cfg = NULL, quiet = FALSE) {
-  tryCatch(
-    {
-      # If this is called in standalone mode, read the config file. Otherwise we expect to have a
-      # `cfg` object passed in
-      if (is.null(cfg)) {
-        cfg <- lambdar_config(lam_config_file())
-      }
-
-      # Validate the config object
-      if (!is_lambdar_config(cfg)) {
-        cli::cli_alert_danger("{.var cfg} must be a {.code lambdar_config} object created by {.fn lambdar_config}")
-        rlang::abort("Invalid configuration object")
-      }
-
-      # Some elements of the config file need formatting before they're inserted into the Dockerfile
-      # template
-      cfg$r_packages      <- lam_build_quoted_list(cfg$r_packages)
-      cfg$r_package_repos <- lam_build_quoted_list(cfg$r_package_repos)
-      cfg$linux_packages  <- lam_build_space_separated_list(cfg$linux_packages)
-      cfg$include_files   <- lam_build_quoted_list(cfg$include_files)
-      cfg$env             <- lam_build_env_list(cfg$env)
-      cfg$r_runtime_file  <- lam_runtime_path()
-
-      # If repos are provided, use them. If not, use `getOption("repos")`
-      if (is.null(cfg$r_package_repos)) {
-        cfg$r_package_repos <- getOption("repos")
-      }
-      cfg$r_package_repos <- lam_build_quoted_list(cfg$r_package_repos)
-
-      # Replace any zero-length elements with NULL to prevent the populating the Dockerfile
-      cfg <- lapply(cfg, function(item) if (length(item) > 0) item else NULL)
-
-      # Build and write the Dockerfile
-      usethis::use_template("Dockerfile", save_as = "Dockerfile", data = cfg, package = "lambdar")
-      if (!quiet) {
-        cli::cli_alert_info("To build your container, run {.code docker build -t {cfg$name} .}")
-      }
-    },
-    lambdar_no_config = function(e) {
-      return(invisible())
-    }
-  )
-}
-
-#' Use lambdar with your project
-#'
-#' @section Description:
-#' Call this function to create the necessary files lambdar needs to work. Specifically:
-#'
-#' * A `_lambdar.yml` config file
-#' * A `lambdar/` directory to house a copy of the R runtime we will load into your container.
-#'
-#' The function will try and pre-populate your config file with as much information as possible, but
-#' you are free to amend as much or as little as you like.
-#'
-#' @export
-use_lambdar <- function() {
-  # If this fails we want to restore the dir to its previous state
-  LAMBDAR_DIR <- lam_dir_path()
-  LAMBDAR_RUNTIME_PATH <- lam_runtime_path()
-
-  tryCatch(
-    {
-      if (!dir.exists(LAMBDAR_DIR)) {
-        dir.create(LAMBDAR_DIR)
-        cli::cli_alert_success("Creating {.path {LAMBDAR_DIR}} directory")
-      }
-      file.copy(
-        system.file("runtime", "lambdar_runtime.R", package = "lambdar", mustWork = TRUE),
-        LAMBDAR_RUNTIME_PATH
-      )
-      cli::cli_alert_success("Writing {.path {LAMBDAR_RUNTIME_PATH}}")
-      build_yaml()
-    },
-    error = function(e) {
-      cli::cli_alert_danger("{e}")
-      unlink(LAMBDAR_DIR, recursive = TRUE, force = TRUE)
-      cli::cli_alert_warning("Removing {.PATH {LAMBDAR_DIR}}")
-      unlink(usethis::proj_get(), "_lambdar.yml", force = TRUE)
-      cli::cli_alert_warning("Removing {.path _lambdar.yml}")
-    }
-  )
-}
-
-#' Build a `_lambdar.yml` file
-#'
-#' Scans your project folder for `.R` files, identified those containing `@lambda` tags, and writes
-#' a `_lambdar.yml` config file containing relevant information.
-#'
-#' @export
-build_yaml <- function() {
-  # TODO: Identify and pre-populate the main file (if it already contains the `@lambdar` tag this
-  #       should be easy), and populate the `include_files` option with a list of everything else in
-  #       the directory that isn't lambdar-related
-
-  # Parse out the handlers ("file.function") and then generate a unique list of "file.R" names
-  handlers <- lam_parse_project_handlers()
-  include_files <- unique(
-    sapply(
-      strsplit(handlers, ".", fixed = T),
-      function(f) paste0(f[[1]], ".R")
-    )
-  )
-
-  data <- list(
-    user = Sys.getenv("USER"),
-    r_version = lam_r_version(),
-    r_package_repos = getOption("repos"),
-    proj_root = basename(usethis::proj_get()),
-    lambda_handlers = lam_build_quoted_list(handlers),
-    include_files = lam_build_quoted_list(include_files)
-  )
-  usethis::use_template("_lambdar.yml", save_as = "_lambdar.yml", data = data, package = "lambdar", open = TRUE)
+  msg <- paste("Once running you can send test queries to",
+               "{.code http://localhost:9000/2015-03-31/functions/function/invocations}")
+  cli::cli_alert_info(msg)
 }
