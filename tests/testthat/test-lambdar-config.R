429b895d36d1102aad442c8b5cf7c5f36653e2be
Lewin Appleton-Fox <lewin.a.f@gmail.com>
2021-08-16 14:23:57 +1200

    Clean up the implementation of `lam_function_exists_in_file()`
    Update tests

--- tests/testthat/test-lambdar-config.R
+++ tests/testthat/test-lambdar-config.R
@@ -1,14 +1,16 @@
 test_that("handler function detection works correctly", {
-  f <- tempfile()
   fn <- "
   find_me <- function() TRUE
 
   not_a_function <- 1
   "
-  cat(fn, file = f)
-  expect_true(lam_function_exists_in_file(f, "find_me"))
-  expect_false(lam_function_exists_in_file(f, "dont_find_me"))
-  expect_message(lam_function_exists_in_file(f, "not_a_function"), regexp = "not a function")
+
+  withr::with_tempfile("f", {
+    cat(fn, file = f)
+    expect_true(lam_function_exists_in_file(f, "find_me"))
+    expect_false(lam_function_exists_in_file(f, "dont_find_me"))
+    expect_false(lam_function_exists_in_file(f, "not_a_function"))
+  })
 })
 
 test_that("`new_lambdar_config()` works as expected", {
