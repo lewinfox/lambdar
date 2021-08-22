357f9c063b18ddde7f196b90ce2b1b2625ca865b
Lewin Appleton-Fox <lewin.a.f@gmail.com>
2021-08-22 23:38:21 +1200

    Add tests for path helpers

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -13,3 +13,14 @@ test_that("`lam_build_space_separated_list()` works correctly", {
 test_that("`lam_build_env_list()` works correctly", {
   expect_equal(lam_build_env_list(list(foo = "bar", baz = "boo")), "FOO=\"bar\" BAZ=\"boo\"")
 })
+
+test_that("file path helpers work", {
+  with_local_project({
+    file.create(lam_dockerfile_path()) # Otherwise `normalizePath()` fails
+    root <- getwd()
+    expect_equal(normalizePath(lam_proj_path()), root)
+    expect_equal(normalizePath(lam_config_path()), file.path(root, "_lambdar.yml"))
+    expect_equal(normalizePath(lam_runtime_path()), file.path(root, ".lambdar", "lambdar_runtime.R"))
+    expect_equal(normalizePath(lam_dockerfile_path()), file.path(root, "Dockerfile"))
+  })
+})
