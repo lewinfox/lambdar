7174325691242ecb2fb8991b5a04e8c5d20ebd2e
Lewin Appleton-Fox <lewin.a.f@gmail.com>
2021-08-22 23:02:52 +1200

    Add `with_local_project()` and silence `usethis` output in tests

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -16,40 +16,61 @@ create_local_thing <- function(dir = fs::file_temp(),
     usethis::ui_stop("Target {ui_code('dir')} {usethis::ui_path(dir)} already exists.")
   }
 
+  withr::with_options(list(usethis.quiet = TRUE), {
+    old_project <- usethis::proj_get() # this could be `NULL`, i.e. no active project
+    old_wd <- getwd()          # not necessarily same as `old_project`
 
-  old_project <- usethis::proj_get() # this could be `NULL`, i.e. no active project
-  old_wd <- getwd()          # not necessarily same as `old_project`
 
-
-  withr::defer(
-    {
-      usethis::ui_done("Deleting temporary project: {usethis::ui_path(dir)}")
-      fs::dir_delete(dir)
-    },
-    envir = env
-  )
-  usethis::ui_silence(
-    switch(
-      thing,
-      package = usethis::create_package(dir, rstudio = rstudio, open = FALSE, check_name = FALSE),
-      project = usethis::create_project(dir, rstudio = rstudio, open = FALSE)
+    withr::defer(
+      {
+        fs::dir_delete(dir)
+      },
+      envir = env
     )
-  )
+    usethis::ui_silence(
+      switch(
+        thing,
+        package = usethis::create_package(dir, rstudio = rstudio, open = FALSE, check_name = FALSE),
+        project = usethis::create_project(dir, rstudio = rstudio, open = FALSE)
+      )
+    )
 
+    withr::defer(usethis::ui_silence(usethis::proj_set(old_project, force = TRUE)), envir = env)
+    usethis::proj_set(dir)
 
-  withr::defer(usethis::proj_set(old_project, force = TRUE), envir = env)
-  usethis::proj_set(dir)
+    withr::defer(
+      {
+        setwd(old_wd)
+      },
+      envir = env
+    )
 
+    setwd(usethis::ui_silence(usethis::proj_get()))
+    invisible(usethis::ui_silence(usethis::proj_get()))
+  })
+}
 
-  withr::defer(
-    {
-      usethis::ui_done("Restoring original working directory: {usethis::ui_path(old_wd)}")
-      setwd(old_wd)
-    },
-    envir = env
-  )
-  setwd(usethis::proj_get())
+# Create a local lambdar probect with config, runtime and a main.R file containing two handlers
+with_local_project <- function(code) {
+  withr::with_options(list(lambdar.quiet = TRUE, usethis.quiet = TRUE), {
+    create_local_thing(thing = "project")
+    init()
+    main_text <- "
+    #' @lambda
+    hello_world <- function(name = NULL) {
+      if (is.null(name)) {
+        name <- 'World'
+      }
+      paste0('Hello, ' name, '!')
+    }
 
-
-  invisible(usethis::proj_get())
+    #' @lambda
+    add_one <- function(x = 0) {
+      x <- as.numeric(x)
+      x + 1
+    }
+    "
+    cat(main_text, file = "main.R")
+    force(code)
+  })
 }
