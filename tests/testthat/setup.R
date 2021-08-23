88fdb921de89cab6fd115c362dba40855d97af70
Lewin Appleton-Fox <lewin.a.f@gmail.com>
2021-08-23 21:17:41 +1200

    Add `options(usethis.quiet = TRUE)` to test setup
    This is a neater way of preventing `usethis` mesages cluttering the console during testing.

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,7 +1,12 @@
+# We don't want usethis printing messages during testing because it clutters the console. See
+# https://community.rstudio.com/t/how-to-test-functions-that-operate-on-projects-without-cluttering-test-output/113283/2
+# for the background
+options(usethis.quiet = TRUE)
+
+
 # Pinched from https://github.com/jimhester/usethis/blob/de8aa116820a8e54f2f952b341039985d78d0352/tests/testthat/helper.R#L28-L68
 # See https://github.com/r-lib/devtools/blob/78b4cabcba47e328e255fffa39e9edcad302f8a0/tests/testthat/test-build-readme.R
 # for an example of how to use.
-
 create_local_thing <- function(dir = fs::file_temp(),
                                env = parent.frame(),
                                rstudio = FALSE,
@@ -16,51 +21,36 @@ create_local_thing <- function(dir = fs::file_temp(),
     usethis::ui_stop("Target {ui_code('dir')} {usethis::ui_path(dir)} already exists.")
   }
 
-  withr::with_options(list(usethis.quiet = TRUE), {
+  # I ran into an issue where R CMD check where the old_project (tests/testthat in the temporary
+  # folder) was not a project so this `proj_get()` call failed with an error. Wrapping this in a
+  # `try()` allows us to A: stop those tests failing and B: check for the existence of an old
+  # project before trying to switch back to one below.
+  old_project <- try(usethis::proj_get(), silent = TRUE)
+  has_old_project <- !inherits(old_project, "try-error")
+  old_wd <- getwd()          # not necessarily same as `old_project`
 
-    # I ran into an issue where R CMD check where the old_project (tests/testthat in the temporary
-    # folder) was not a project so this `proj_get()` call failed with an error. Wrapping this in a
-    # `try()` allows us to A: stop those tests failing and B: check for the existence of an old
-    # project before trying to switch back to one below.
-    old_project <- try(usethis::proj_get(), silent = TRUE)
-    has_old_project <- !inherits(old_project, "try-error")
-    old_wd <- getwd()          # not necessarily same as `old_project`
+  withr::defer(fs::dir_delete(dir), envir = env)
 
+  switch(
+    thing,
+    package = usethis::create_package(dir, rstudio = rstudio, open = FALSE, check_name = FALSE),
+    project = usethis::create_project(dir, rstudio = rstudio, open = FALSE)
+  )
 
-    withr::defer(
-      {
-        fs::dir_delete(dir)
-      },
-      envir = env
-    )
-    usethis::ui_silence(
-      switch(
-        thing,
-        package = usethis::create_package(dir, rstudio = rstudio, open = FALSE, check_name = FALSE),
-        project = usethis::create_project(dir, rstudio = rstudio, open = FALSE)
-      )
-    )
+  if (has_old_project) {
+    withr::defer(usethis::proj_set(old_project, force = TRUE), envir = env)
+  }
+  usethis::proj_set(dir)
 
-    if (has_old_project) {
-      withr::defer(usethis::ui_silence(usethis::proj_set(old_project, force = TRUE)), envir = env)
-    }
-    usethis::proj_set(dir)
+  withr::defer(setwd(old_wd), envir = env)
 
-    withr::defer(
-      {
-        setwd(old_wd)
-      },
-      envir = env
-    )
-
-    setwd(usethis::ui_silence(usethis::proj_get()))
-    invisible(usethis::ui_silence(usethis::proj_get()))
-  })
+  setwd(usethis::proj_get())
+  invisible(usethis::proj_get())
 }
 
-# Create a local lambdar probect with config, runtime and a main.R file containing two handlers
+# Create a local lambdar project with config, runtime and a main.R file containing two handlers
 with_local_project <- function(code) {
-  withr::with_options(list(lambdar.quiet = TRUE, usethis.quiet = TRUE), {
+  withr::with_options(list(lambdar.quiet = TRUE), {
     create_local_thing(thing = "project")
     init()
     main_text <- "
